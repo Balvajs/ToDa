9d966ade8f4b8808fa16846b5ca26d7af261f829
Tomas Balvin <balvintomas@gmail.com>
2024-09-11 16:22:24 +0200

    fix types

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,31 +2,33 @@ import {
   DocumentHeadTags,
   documentGetInitialProps,
 } from '@mui/material-nextjs/v14-pagesRouter';
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import type { DocumentHeadTagsProps } from '@mui/material-nextjs/v14-pagesRouter';
+import { Head, Html, Main, NextScript } from 'next/document';
+import type { DocumentProps, DocumentContext } from 'next/document';
 
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          <DocumentHeadTags {...this.props} />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function MyDocument(
+  props: DocumentProps & DocumentHeadTagsProps,
+) {
+  return (
+    <Html>
+      <Head>
+        <DocumentHeadTags {...props} />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
 
-MyDocument.getInitialProps = async (ctx) => {
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
   const finalProps = await documentGetInitialProps(ctx);
   return finalProps;
 };
